ef6c1e78ec74ada8965a068ee4ea4a546e386f74
Joe Symons <joe.j.symons@gmail.com>
2024-03-06 13:45:54 +0000

    Testing succesful. Adding Comments.

--- readability.py
+++ readability.py
@@ -1,5 +1,7 @@
 """
+
     Problem to Solve
+
 According to Scholastic, E.B. White's Charlotte's Web is between a second- and fourth-grade reading level, and Lois 
 Lowry's The Giver is between an eighth- and twelfth-grade reading level. What does it mean, though, for a book to 
 be at a particular reading level?
@@ -13,7 +15,9 @@ the grade level computed, rounded to the nearest integer. If the grade level is 
 greater than a senior undergraduate reading level), your program should output “Grade 16+” instead of giving the 
 exact index number. If the grade level is less than 1, your program should output “Before Grade 1”.
 
+
     Background
+
 So what sorts of traits are characteristic of higher reading levels? Well, longer words probably correlate with 
 higher reading levels. Likewise, longer sentences probably correlate with higher reading levels, too.
 
@@ -23,25 +27,80 @@ to output that (U.S.) grade level that is needed to understand some text. The fo
 
 index = 0.0588 * L - 0.296 * S - 15.8
 where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
+
+
+    How to Test
+
+Try running your program on the following texts, to ensure you see the specified grade level. Be sure to copy only 
+the text, no extra spaces.
+
+One fish. Two fish. Red fish. Blue fish. 
+(Before Grade 1)
+Would you like them here or there? I would not like them here or there. I would not like them anywhere. 
+(Grade 2)
+Congratulations! Today is your day. You're off to Great Places! You're off and away! 
+(Grade 3)
+Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any 
+other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the 
+dead of the night. And he also happened to be a wizard. 
+(Grade 5)
+In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever 
+since. 
+(Grade 7)
+Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or 
+twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, "and what 
+is the use of a book," thought Alice "without pictures or conversation?" 
+(Grade 8)
+When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow. When it healed, and Jem's fears 
+of never being able to play football were assuaged, he was seldom self-conscious about his injury. His left arm was 
+somewhat shorter than his right; when he stood or walked, the back of his hand was at right angles to his body, his 
+thumb parallel to his thigh. 
+(Grade 8)
+There are more things in Heaven and Earth, Horatio, than are dreamt of in your philosophy. 
+(Grade 9)
+It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his 
+breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though 
+not quickly enough to prevent a swirl of gritty dust from entering along with him. 
+(Grade 10)
+A large class of computational problems involve the determination of properties of graphs, digraphs, integers, 
+arrays of integers, finite families of finite sets, boolean formulas and elements of other countable domains. 
+(Grade 16+)
 """
 
 import re
 
 def main():
-    text = input("Text: ").split(".")
-    text = remove_non_alpha(text)
-        
 
-def remove_non_alpha(input_list):
-    return [''.join(letter for letter in word if letter.isalpha() or letter.isspace()).strip() for word in input_list]
+    text = input("Text: ")
+    text = text.replace("\"", "")
 
-main()
+    sentences = re.split(r'[.?!]', text)
+    if sentences[-1] == "":
+        sentences.pop()
+    sentences = len(sentences)
+    
+    letters = 0
+
+    for i in text:
+        if i.isalpha():
+            letters += 1
+    
+    words = len(text.split(' '))
 
 
-"""
-    // Count the number of letters, words, and sentences in the text
+    # Coleman index
 
-    // Compute the Coleman-Liau index
+    L = (letters/words) * 100
+    S = (sentences/words) * 100
 
-    // Print the grade level
-    """
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+    print(index)
+
+    if index < 1:
+        return "Before Grade 1"
+    elif index < 16:
+        return f"Grade {index}" 
+    else: 
+        return "Grade 16+"
+
+print(main())
